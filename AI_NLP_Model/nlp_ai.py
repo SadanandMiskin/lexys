a8ceb48914c49cdca7ca86398e903a5879a37a91
VaibahvVernekar27 <149693400+VaibahvVernekar27@users.noreply.github.com>
2023-11-02 22:50:07 +0530

    Preprocessing function using regular expression

--- AI_NLP_Model/nlp_ai.py
+++ AI_NLP_Model/nlp_ai.py
@@ -2,3 +2,13 @@ import pandas as pd
 df= pd.read_csv("/content/Dateset_NLP.csv", names=["Text", "Labels"],header=None)
 print(df.shape)
 df.head(3)
+
+import re
+def Preprocess(text):
+    # Remove extra white spaces, tabs, and line breaks
+    text = re.sub('\s+', ' ', text)
+
+    # Remove special characters and punctuation
+    text = re.sub(r'[^\w\s]', '', text)
+
+    return text
