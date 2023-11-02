e2cc813006227bf7d19f4b2a46fc6c7b2ae14bff
ApoorvaAppu12 <139629968+ApoorvaAppu12@users.noreply.github.com>
2023-11-02 22:55:53 +0530

    Importing Bert for Tokenizer and Sequence  Classification for tokenizing the dataset

--- AI_NLP_Model/nlp_ai.py
+++ AI_NLP_Model/nlp_ai.py
@@ -12,3 +12,27 @@ def Preprocess(text):
     text = re.sub(r'[^\w\s]', '', text)
 
     return text
+
+from tranformers import BertTokenizer, BertForSequenceClassification, BertForMaskedLM
+
+class LegalDocumentDataset(Dataset):
+    def _init_(self, texts, labels, tokenizer, max_length):
+        self.texts = texts
+        self.labels = labels
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+
+    def _len_(self):
+        return len(self.texts)
+
+    def _getitem_(self, idx):
+        text = self.texts[idx]
+        label = self.labels[idx]
+        encoding = self.tokenizer(
+            text, padding='max_length', truncation=True, max_length=self.max_length, return_tensors='pt'
+        )
+        return {
+            'input_ids': encoding['input_ids'].flatten(),
+            'attention_mask': encoding['attention_mask'].flatten(),
+            'labels': label
+        }
