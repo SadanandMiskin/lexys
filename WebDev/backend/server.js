557ed463d98ba4c9942cdfe2852a85dde1c06722
Sadanand Miskin <miskinsadanand@gmail.com>
2023-11-03 14:18:18 +0530

    setup mongodb and node environment

---
+++ WebDev/backend/server.js
@@ -0,0 +1,35 @@
+//Jai Shree Ram
+const main = require('./routes/main')
+const register = require('./routes/register')
+const login = require('./routes/login')
+const dashboard = require('./routes/dashboard')
+require('./config/db')
+
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+const session = require('express-session')
+const app = express()
+
+app.use(session({
+    secret: 'lexmachinamachinalex',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+  }))
+
+
+
+app.use(express.static(__dirname + '/public/'))
+app.set('view engine', 'ejs')
+app.set('views' ,path.join(__dirname, 'views'))
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', main)
+app.use('/register', register)
+app.use('/login' , login)
+app.use('/dashboard' , dashboard)
+
+app.listen(3000, ()=>{
+    console.log('Server listening')
+})
